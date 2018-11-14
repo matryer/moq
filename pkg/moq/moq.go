2dff27bb3d9876d633f37d5ec9623fabad3a2e54
Roman Levin <romanlevin@users.noreply.github.com>
2018-11-14 14:10:48 +0100

    Use default GOPATH if env variable not set
    `moq` currently breaks in environments where `$GOPATH` is not set:
    
    ```
    import "/Users/user/go/src/github.com/somepackage": cannot import absolute path
    no initial packages were loaded
    ```

--- pkg/moq/moq.go
+++ pkg/moq/moq.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"go/ast"
+	"go/build"
 	"go/format"
 	"go/parser"
 	"go/token"
@@ -354,5 +355,9 @@ func stripGopath(p string) string {
 }
 
 func gopaths() []string {
-	return strings.Split(os.Getenv("GOPATH"), string(filepath.ListSeparator))
+	gopath := os.Getenv("GOPATH")
+    	if gopath == "" {
+        	gopath = build.Default.GOPATH
+    	}
+	return strings.Split(gopath, string(filepath.ListSeparator))
 }
