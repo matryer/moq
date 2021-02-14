a18e99c40627db2e58e175d69776e6fb4d7036d7
Suhas Karanth <sudo.suhas@gmail.com>
2021-02-14 11:43:15 +0530

    Fix var name generation to avoid conflict
    When the type and the package name is the same for an anonymous
    parameter (ex: time.Time), and there are more than 1 such parameters,
    the generated name for both was the same. And the generated code would
    not be valid.
    
    Fix the bug by ensuring the parameter name does not conflict with
    package imports first before checking against other parameter names.

--- pkg/moq/testpackages/paramconflict/iface.go
+++ pkg/moq/testpackages/paramconflict/iface.go
@@ -1,5 +1,7 @@
 package paramconflict
 
+import "time"
+
 type Interface interface {
-	Method(string, bool, string, bool, int, int32, int64, float32, float64)
+	Method(string, bool, string, bool, int, int32, int64, float32, float64, time.Time, time.Time)
 }
