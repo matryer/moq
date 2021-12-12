57ca0f2a8afec9c4c44bec0d21fe03c7f9dc4f89
Jacopo <dedalusj@gmail.com>
2021-12-12 10:18:26 +1000

    Fix issue with custom types shadowing basic types

---
+++ pkg/moq/testpackages/shadowtypes/types/types.go
@@ -0,0 +1,29 @@
+package types
+
+type String string
+
+type Int int
+type Int8 int8
+type Int16 int16
+type Int32 int32
+type Int64 int64
+
+type Uint uint
+type Uint8 uint
+type Uint16 uint
+type Uint32 uint
+type Uint64 uint
+
+type Float32 float32
+type Float64 float64
+
+type Byte byte
+
+type Rune rune
+
+type Bool bool
+
+type Complex64 complex64
+type Complex128 complex128
+
+type Uintptr uintptr
