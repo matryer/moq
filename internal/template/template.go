b7e1bfbdbce11db472427324db9b9c982bfcdc62
testuser64920 <126826982+testuser64920@users.noreply.github.com>
2023-03-06 10:06:48 -0500

    improves naming conventions

--- internal/template/template.go
+++ internal/template/template.go
@@ -182,8 +182,8 @@ func (mock *{{$mock.MockName}}
 	return calls
 }
 {{- if $.WithResets}}
-// {{.Name}}ResetCalls Reset all the calls that were made to {{.Name}}.
-func (mock *{{$mock.MockName}}) {{.Name}}ResetCalls() {
+// Reset{{.Name}}Calls reset all the calls that were made to {{.Name}}.
+func (mock *{{$mock.MockName}}) Reset{{.Name}}Calls() {
 	mock.lock{{.Name}}.Lock()
 	mock.calls.{{.Name}} = nil
 	mock.lock{{.Name}}.Unlock()
