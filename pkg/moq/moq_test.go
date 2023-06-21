3795b7405d73c5e29806ec118a425ee79f5f4b4e
Tim van Osch <timvanosch@pollex.nl>
2023-06-21 11:01:30 +0200

    Add imports for Named Type's type arguments (#199)

--- pkg/moq/moq_test.go
+++ pkg/moq/moq_test.go
@@ -390,6 +390,12 @@ func TestMockGolden(t *testing.T) {
 			goldenFile: filepath.Join("testpackages/generics", "generics_moq.golden.go"),
 		},
 		{
+			name:       "Generic Return Argument Type",
+			cfg:        Config{SrcDir: "testpackages/genericreturn"},
+			interfaces: []string{"IFooBar"},
+			goldenFile: filepath.Join("testpackages/genericreturn", "genericreturn.golden.go"),
+		},
+		{
 			name:       "TransientImport",
 			cfg:        Config{SrcDir: "testpackages/transientimport"},
 			interfaces: []string{"Transient"},
@@ -659,7 +665,8 @@ func TestParseError(t *testing.T) {
 		t.Errorf("expected error but got nil")
 		return
 	}
-	if !strings.Contains(err.Error(), `could not import github.com/matryer/notexist (invalid package name: "")`) {
+	// The first clause is the common error. Only in go version 1.19.10 the error differs
+	if !strings.Contains(err.Error(), `could not import github.com/matryer/notexist (invalid package name: "")`) && !strings.Contains(err.Error(), "stderr: go build github.com/matryer/notexist") {
 		t.Errorf("unexpected error: %s", err.Error())
 	}
 }
